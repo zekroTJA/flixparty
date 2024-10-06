278147e5a3c79c004b97ab43721fe31ed4b4a36c
zekroTJA <riho@live.de>
2024-10-06 13:17:25 +0200

    WIP: eliminate enigo dependency

--- src/clicker.rs
+++ src/clicker.rs
@@ -1,23 +1,15 @@
 use anyhow::Result;
-use enigo::{Button, Coordinate, Direction, Enigo, Mouse, Settings};
+use rdev::{simulate, EventType, Key};
 
-pub struct Clicker {
-    handler: Enigo,
-}
+pub struct Clicker {}
 
 impl Clicker {
     pub fn new() -> Result<Self> {
-        let handler = Enigo::new(&Settings::default())?;
-        Ok(Self { handler })
+        Ok(Self {})
     }
 
     pub fn click_center(&mut self) -> Result<()> {
-        let (width, height) = self.handler.main_display()?;
-        let (curr_x, curr_y) = self.handler.location()?;
-        self.handler
-            .move_mouse(width / 2, height / 2, Coordinate::Abs)?;
-        self.handler.button(Button::Left, Direction::Click)?;
-        self.handler.move_mouse(curr_x, curr_y, Coordinate::Abs)?;
+        simulate(&EventType::KeyPress(Key::Space))?;
         Ok(())
     }
 }
