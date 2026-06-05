4b9a425474f948da90f6a3c46f037c3746cb6b61
zekroTJA <riho@live.de>
2026-06-05 20:21:12 +0200

    fix fallback focus function

--- src/condition/mod.rs
+++ src/condition/mod.rs
@@ -3,7 +3,7 @@ cfg_if::cfg_if! {
         mod windows;
         pub use windows::*;
     } else {
-        pub fn is_browser_in_focus(matcher: Option<crate::config::Condition>) -> bool {
+        pub fn is_browser_in_focus(matcher: Option<&crate::config::Condition>) -> bool {
             if matcher.is_some() {
                 tracing::warn!("conditions are not supported in this platform");
             }
